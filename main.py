e6df9fe97c16564a82dc5b4fc9a3bbc2c5d1338d
João Alves <joao.alves64@gmail.com>
2022-11-28 20:53:12 -0300

    add file_exists fuction

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# This is a sample Python script.
-
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import os
+import uuid
 import shutil
 import pandas as pd
+from os.path import exists
 
 def adjust_name(name):
     name = name.replace('austallia','australia')
@@ -16,25 +13,39 @@ def mkdirs(directory, mode=0o777):
         os.makedirs(directory, mode)
     except OSError as err:
         return err
+def file_exists(file_path, file_name):
+    if exists(file_path):
+        file_name_new = f"{file_name}-{str(uuid.uuid4().fields[-1])[:5]}"
+        file_path = file_path.replace(file_name,file_name_new)
+        return file_path,file_name_new
+    return file_path,file_name
 
 
 data = []
 
 path = "assets/flags"
 
-for folder in os.listdir(path):
-    if folder != ".DS_Store":
-        for item in os.listdir(f"{path}/{folder}"):
-            new_item = item.split("-", 1)
-            new_item = adjust_name(new_item[0])
-            mkdirs(f"assets/flags_new/{folder}")
-            original = f"{path}/{folder}/{item}"
-            target = f"assets/flags_new/{folder}/{new_item}.png"
-            shutil.copyfile(original, target)
-            data.append([new_item,f"{new_item}.png",target,folder,f"https://raw.githubusercontent.com/joaonart/country_flags/master/assets/flags_new/{folder}/{new_item}.png"])
+remote_path_base = "https://raw.githubusercontent.com/joaonart/country_flags/master/assets/flags_new"
 
-df = pd.DataFrame(data, columns=['Country','Flag','Path','Folder','WebPath'])
+try:
 
-df.to_csv("datasets/country_flags_dataset.csv")
+    for folder in os.listdir(path):
+        if folder != ".DS_Store":
+            for item in os.listdir(f"{path}/{folder}"):
+                new_item = item.split("-", 1)
+                new_item = adjust_name(new_item[0])
+                mkdirs(f"assets/flags_new/{folder}")
+                original = f"{path}/{folder}/{item}"
+                target = f"assets/flags_new/{folder}/{new_item}.png"
+                target,new_item = file_exists(target,new_item)
+                shutil.copyfile(original, target)
+                data.append([new_item,f"{new_item}.png",target,folder,f"{remote_path_base}/{folder}/{new_item}.png"])
 
-print(df)
+    df = pd.DataFrame(data, columns=['Country','Flag','Path','Folder','WebPath'])
+
+    df.to_csv("datasets/country_flags_dataset.csv")
+
+    print("Finished process!!!")
+
+except OSError as error:
+    print(error)
